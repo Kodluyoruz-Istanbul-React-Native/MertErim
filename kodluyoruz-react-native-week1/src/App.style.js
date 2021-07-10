38b98b8db9861446caa6cee0f7e1897a2a75da3a
wirdes <45904810+wirdes@users.noreply.github.com>
2021-07-10 09:57:10 +0300

    first commit

---
+++ kodluyoruz-react-native-week1/src/App.style.js
@@ -0,0 +1,12 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  safeAreaView: {
+    flex: 1,
+  },
+  productText: {
+    textAlign: 'center',
+    fontSize: 55,
+    fontWeight: 'bold',
+  },
+});
---
+++ kodluyoruz-react-native-week1/src/components/UserCard/UserCard.style.js
@@ -0,0 +1,42 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: '#eceff9',
+    margin: 5,
+    padding: 10,
+    borderRadius: 5,
+    borderTopLeftRadius: 55,
+    borderBottomLeftRadius: 55,
+    borderWidth: 100,
+    borderColor: 'gray',
+    flexDirection: 'row',
+  },
+  username: {
+    fontSize: 25,
+    fontWeight: 'bold',
+  },
+  product_container: {
+    flexDirection: 'row',
+  },
+  product_image: {
+    marginTop: 10,
+    borderRadius: 50,
+    width: 100,
+    height: 100,
+  },
+  price_container: {
+    flex: 1,
+    alignItems: 'flex-end',
+    color: 'green',
+  },
+  price_text: {
+    marginTop: 10,
+    color: 'green',
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  description_text: {
+    fontSize: 15,
+  },
+});
