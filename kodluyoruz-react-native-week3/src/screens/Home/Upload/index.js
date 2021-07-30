0ee4e6cb3805d87022490466e925945c3f30f48c
wirdes <45904810+wirdes@users.noreply.github.com>
2021-07-30 18:06:20 +0300

    week3

---
+++ kodluyoruz-react-native-week3/src/screens/Home/Upload/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Upload = props => {
+  return (
+    <View>
+      <Text>Upload </Text>
+    </View>
+  );
+};
+
+export default Upload;
